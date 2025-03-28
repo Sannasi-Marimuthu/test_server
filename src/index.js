0d48dd13a2c0c770a2537373185e3132c40678d3
sannasi marimuthu <sannasimarimuthu21@gmail.com>
2025-03-28 13:02:47 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+
+
+dotenv.config()
+connectDB()
+const PORT = 5500
+
+const app = express()
+
+//middleware
+app.use(cors())  
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(req,res)=> {
+    res.send("<h1>san</h1>")
+})
+
+app.listen(PORT,()=> console.log(`Server started on port ${PORT}`))
