8fc059c055c79720091075729a3ba25a367bf151
sannasi marimuthu <sannasimarimuthu21@gmail.com>
2025-04-11 12:48:31 +0530

    swagger documentation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 import postRoutes from "./routes/postRoutes.js"
 
+import Yaml from 'yamljs';
+import swaggerUI from 'swagger-ui-express';
+
+const yamlFilePath = "./src/api.yaml"
+const swaggerJSDocs = Yaml.load(yamlFilePath)
+
+
+
 dotenv.config()
 connectDB()
 const PORT = 5500
@@ -15,6 +23,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use("/api", postRoutes)
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSDocs))
 app.get("/",(req,res)=> {
     res.send("<h1>san</h1>")
 })
